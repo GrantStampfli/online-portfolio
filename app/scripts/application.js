d915d7477d37575a9baa5c85220c9c59e0c83bd2
Grant Stampfli <mail@gstampfli.com>
2014-08-02 12:37:39 -0700

    lazy loader

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,5 +1,12 @@
 'use strict';
 
-$(function() {
-  // your code here
+$(function () {
+	$(document).spinload();
+	$(document).placeholder();
+	$(window).bind("load resize", function (event) {
+		$(".interface").css({"margin": $(window).height() + "px 0 0 0"});
+	});
+	$(window).bind("load resize scroll", function (event) {
+		$(".interface").effect();
+	});
 });

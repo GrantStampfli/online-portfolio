40a597a94e8f3f0d12a3c91ebe30c3c16b9f8df5
Grant Stampfli <mail@gstampfli.com>
2014-08-03 17:39:25 -0700

    working on background image sclide show

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,6 +1,17 @@
 'use strict';
 
 $(function () {
+  $("#gallery").find(".slideshow").each(function (index) {
+    var that = this;        
+    $(that).anim();
+  });
+  $("#gallery").find(".animation").each(function (index) {
+    var that = this;        
+    $(that).engine();
+  });
+});
+
+$(function () {
 	$(document).spinload();
 	$(document).placeholder();
 	$(window).bind("load resize", function (event) {

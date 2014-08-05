84216b469b041c6bcf9dab8a3a5c4a9b3d15d86c
Grant Stampfli <mail@gstampfli.com>
2014-08-05 14:33:56 -0700

    got rid of gulp because it was slowing me down. my gulp kept crashing

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -12,12 +12,12 @@ $(function () {
 });
 
 $(function () {
-	$(document).spinload();
-	$(document).placeholder();
-	$(window).bind("load resize", function (event) {
-		$(".interface").css({"margin": $(window).height() + "px 0 0 0"});
-	});
-	$(window).bind("load resize scroll", function (event) {
-		$(".interface").effect();
-	});
+  $(document).spinload();
+  $(document).placeholder();
+  $(window).bind("load resize", function (event) {
+    $(".interface").css({"margin": $(window).height() + "px 0 0 0"});
+  });
+  $(window).bind("load resize scroll", function (event) {
+    $(".interface").effect();
+  });
 });
